db1364b40f3404b4cd703782dd506c9b1b16d14b
s20612 <s20612@pjwstk.edu.pl>
2020-01-31 09:05:25 +0100

    zmiany zmiany zmiany 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Odliczanie do wyników kolokwium!");
+        for(int i=10; i>0; i--){
+            System.out.println(i);
+        }
+        System.out.println("Wesołego ITN-a!");
     }
 }
